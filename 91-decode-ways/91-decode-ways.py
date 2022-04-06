d012bcc5584706e4e4b4a73ec46511c6e9f65edc
Pat <ankitfirebolt@gmail.com>
2022-04-06 13:10:05 -0400

    Time: 24 ms (70.48%), Space: 14.5 MB (5.95%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -21,10 +21,10 @@ class Solution(object):
             count_1, count_2 = 0, 0
             
             if int(s[0]) in allow_set:
-                count_1 = helper(s[1:])
+                count_1 = helper(s[1:], memo)
                 memo[s[1:]] = count_1
             if len(s)>=2 and int(s[:2]) in allow_set:
-                count_2 = helper(s[2:])
+                count_2 = helper(s[2:], memo)
                 memo[s[2:]] = count_2
 
             return count_1 + count_2

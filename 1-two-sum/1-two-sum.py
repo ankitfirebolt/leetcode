05bc55c138464f8b0838aea7ac8c4d21cf060900
Pat <ankitfirebolt@gmail.com>
2022-09-08 13:19:21 -0400

    Time: 140 ms (39.25%), Space: 15.2 MB (49.59%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashdict = {}
-        
+        seen_dict = {}
         for i,n in enumerate(nums):
-            if target - n in hashdict:
-                return [i, hashdict[target-n]]
-            hashdict[n]=i
-            
+            if target-n in seen_dict:
+                return [seen_dict[target-n], i]
+            else:
+                seen_dict[n] = i

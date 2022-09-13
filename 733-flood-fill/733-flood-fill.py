1fb9308a3827887590cacd9dd3ff5a50e8e2c16f
Pat <ankitfirebolt@gmail.com>
2022-09-13 15:49:28 -0400

    Time: 81 ms (90.65%), Space: 14.1 MB (37.90%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,21 +1,20 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
-        curr_color = image[sr][sc]
-        if curr_color == color:
-            return image
+        curr_color, new_color = image[sr][sc], color
+        if curr_color == new_color:
+            return image #otherwise we will need to flag visited nodes to avoid infinite recursion
         
-        def dfs(image, r, c, curr_color, new_color):
+        def dfs(r, c):
             if r < 0 or r >= len(image) or c < 0 or c >= len(image[0]) or image[r][c] != curr_color:
                 return
             elif image[r][c] == curr_color:
                 image[r][c] = new_color
                 #dfs on neighbors
-                dfs(image, r+1, c, curr_color, new_color)
-                dfs(image, r-1, c, curr_color, new_color)
-                dfs(image, r, c+1, curr_color, new_color)
-                dfs(image, r, c-1, curr_color, new_color)
+                dfs(r+1, c)
+                dfs(r-1, c)
+                dfs(r, c+1)
+                dfs(r, c-1)
 
-
-        dfs(image, sr, sc, curr_color, color)
+        dfs(sr, sc)
         return image

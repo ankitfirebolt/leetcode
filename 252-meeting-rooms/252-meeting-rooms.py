80c3f1fb595c39cfc8c8a1e53085adc54b4c64b2
Pat <ankitfirebolt@gmail.com>
2022-09-24 14:32:24 -0400

    Time: 166 ms (21.59%), Space: 17.7 MB (7.70%) - LeetHub

---
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        
+        sorted_intervals = sorted(intervals, key = lambda k : k[0])
+        
+        new_list = []
+        
+        for k in sorted_intervals:
+            new_list += k
+            
+        print(new_list)
+        return new_list == sorted(new_list)
